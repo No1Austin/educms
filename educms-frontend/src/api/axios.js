5626c753fc39577455a9ad4e2ba3bc3f00100a45
No1Austin <amadiaustin@rocketmail.com>
2026-04-12 22:01:28 -0400

    1stupdate

--- educms-frontend/src/api/axios.js
+++ educms-frontend/src/api/axios.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api/v1',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api/v1',
 });
 
-// 🔥 ADD THIS
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
 
